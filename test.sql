eb705a70ae193c171bf7b0785dba6c20a45010ce
VeeraDinesh <veeradinesh7080@gmail.com>
2022-10-28 13:56:26 +0530

    test.sql

---
+++ test.sql
@@ -0,0 +1,43 @@
+/* CREATE TABLE1 */
+
+CREATE TABLE customers (
+customer_id INT,
+Iname VARCHAR(100),
+fname VARCHAR(100),
+website VARCHAR(100)
+);
+
+/* INSERT QUERY NO: 1 */
+INSERT INTO customers (customer_id, Iname, fname, website)
+VALUES
+(
+401, 'singh', 'Dolly', 'abc.com'
+);
+
+/* INSERT QUERY NO: 2 */
+INSERT INTO customers (customer_id, Iname, fname, website)
+VALUES
+(
+402, 'Chauhan', 'Anuj', 'def.com'
+);
+
+/* INSERT QUERY NO: 3 */
+INSERT INTO customers (customer_id, Iname, fname, website)
+VALUES
+(
+403, 'Kumar', 'Niteesh', 'ghi.com'
+);
+
+/* INSERT QUERY NO: 4 */
+INSERT INTO customers (customer_id, Iname, fname, website)
+VALUES
+(
+404, 'Gupta', 'Shubham', 'jkl.com'
+);
+
+/* INSERT QUERY NO: 5 */
+INSERT INTO customers (customer_id, Iname, fname, website)
+VALUES
+(
+405, 'Walecha', 'Divya', 'abc.com'
+);
